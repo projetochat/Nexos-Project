e576cdee8417d9a9ddbc04c5f6fade8e301a3f99
Natã Rabelo <106699779+NataRabelo@users.noreply.github.com>
2026-07-29 21:58:17 -0300

    chore: remove protected record module

--- backend/src/tenant-records/tenant-records.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AuthModule } from "../auth/auth.module";
-import { TenantRecordsController } from "./tenant-records.controller";
-
-@Module({
-  imports: [AuthModule],
-  controllers: [TenantRecordsController],
-})
-export class TenantRecordsModule {}
